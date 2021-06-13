ad0d7ea61ea0b568ba43a78ad020a124b14a1267
Oebu Petru <anrei.bors@gmail.com>
2021-06-13 17:35:32 +0300

    school

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-export const setLoaded = payload =>({
+export const setLoaded = payload => ({
     type: "SET_LOADING",
     payload
 })
 
 
-export const fetchPizzas = (sortBy, category) => (dispatch) =>{
-    dispatch(setLoaded(false))
-    axios.get(`http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({ data }) => {
+export const fetchPizzas = (sortBy, category) => (dispatch) => {
+        dispatch(setLoaded(false))
+
+        axios.get(`https://run.mocky.io/v3/d58511e1-9c31-490c-bb52-c79f7c21b6b4?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({ data }) => {
         dispatch(setPizzas(data));
     });
 }
